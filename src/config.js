8d5b38c38a88ac28574df35c56af6a3e4a5c9976
Diego García <diego.garcia.segura@gmail.com>
2017-10-13 17:20:16 -0600

    Test

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const graphUri = 'https://globesqlserver.herokuapp.com/graphql';
-//export const graphUri = process.env.NODE_ENV === 'production' ? 'https://globesqlserver.herokuapp.com/graphql' : 'http://localhost:4000/graphql';
+export const graphUri = 'https://globesqlserver.herokuapp.com:5000/graphql';
+//export const graphUri = 'http://localhost:4000/graphql';
+//export const graphUri = process.env.NODE_ENV === 'production' ? 'https://globesqlserver.herokuapp.com:5000/graphql' : 'http://localhost:4000/graphql';
